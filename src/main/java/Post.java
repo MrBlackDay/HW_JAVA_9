678ebe1f8b881336b9d3b862dab14b6c0665c74a
Dima Lis <iselis777888999@gmail.com>
2023-07-27 12:42:34 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; //поле для серии и номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для отметки факта подписки
+    public FormDate birthday;
+
+
+
+
+
+}
